57add23e37e2b664ef3366fee4d52c1d2ea3a7da
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2024-12-31 22:03:46 +0600

    union type added

--- anisul_islam_ts/output/js/main.js
+++ anisul_islam_ts/output/js/main.js
@@ -1,15 +1,32 @@
 "use strict";
-// built-in-data-type: string, number, boolean, void, null, undefined
-let userId;
+// built-in-data-type: string, number, boolean, void, null, undefined  💚💚💚💛
+// let userId: number;
+// let firstName: string;
+// let lastName: string;
+// let fullName: string;
+// let isSkilled: boolean;
+// userId = 101;
+// firstName = 'Mohammad';
+// lastName = ' Ibrahim';
+// isSkilled = true;
+// fullName = firstName.concat( lastName);
+// console.log(fullName);
+// console.log(`My name is ${fullName}`);
+// console.log(fullName.split(' '));
+// union-data-type: string | number | boolean  💚💚💚💛
+// let userInfo: (string | number | boolean);
+// userInfo = 'ohid';
+// userInfo = 27;
+// userInfo = true;
+// function person(userId:string | number){
+//     console.log(userId);
+// }
+// person('101');
+// person(2);
+// array type  💚💚💚💛
+let userName;
+userName = ['ibrahim', 'ohid'];
+console.log(userName);
 let firstName;
-let lastName;
-let fullName;
-let isSkilled;
-userId = 101;
-firstName = 'Mohammad';
-lastName = ' Ibrahim';
-isSkilled = true;
-fullName = firstName.concat(lastName);
-console.log(fullName);
-console.log(`My name is ${fullName}`);
-console.log(fullName.split(' '));
+firstName = ['Mohammad Ibrahim'];
+console.log(firstName);
--- anisul_islam_ts/src/main.ts
+++ anisul_islam_ts/src/main.ts
@@ -1,17 +1,40 @@
-// built-in-data-type: string, number, boolean, void, null, undefined
+// built-in-data-type: string, number, boolean, void, null, undefined  💚💚💚💛
 
-let userId: number;
-let firstName: string;
-let lastName: string;
-let fullName: string;
-let isSkilled: boolean;
+// let userId: number;
+// let firstName: string;
+// let lastName: string;
+// let fullName: string;
+// let isSkilled: boolean;
 
-userId = 101;
-firstName = 'Mohammad';
-lastName = ' Ibrahim';
-isSkilled = true;
+// userId = 101;
+// firstName = 'Mohammad';
+// lastName = ' Ibrahim';
+// isSkilled = true;
 
-fullName = firstName.concat( lastName);
-console.log(fullName);
-console.log(`My name is ${fullName}`);
-console.log(fullName.split(' '));
+// fullName = firstName.concat( lastName);
+// console.log(fullName);
+// console.log(`My name is ${fullName}`);
+// console.log(fullName.split(' '));
+
+// union-data-type: string | number | boolean  💚💚💚💛
+
+// let userInfo: (string | number | boolean);
+// userInfo = 'ohid';
+// userInfo = 27;
+// userInfo = true;
+
+// function person(userId:string | number){
+//     console.log(userId);
+// }
+// person('101');
+// person(2);
+
+// array type  💚💚💚💛
+
+let userName: string[];
+userName = ['ibrahim', 'ohid'];
+console.log(userName);
+
+let firstName: Array<string>;
+firstName = ['Mohammad Ibrahim'];
+console.log(firstName);
