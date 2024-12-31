a52120632b5d812412121de631ae38ad546a48d3
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2024-12-31 21:30:33 +0600

    built in data type practice

--- anisul_islam_ts/output/js/main.js
+++ anisul_islam_ts/output/js/main.js
@@ -1,2 +1,15 @@
 "use strict";
-let y = 2;
+// built-in-data-type: string, number, boolean, void, null, undefined
+let userId;
+let firstName;
+let lastName;
+let fullName;
+let isSkilled;
+userId = 101;
+firstName = 'Mohammad';
+lastName = ' Ibrahim';
+isSkilled = true;
+fullName = firstName.concat(lastName);
+console.log(fullName);
+console.log(`My name is ${fullName}`);
+console.log(fullName.split(' '));
--- anisul_islam_ts/src/main.ts
+++ anisul_islam_ts/src/main.ts
@@ -1,1 +1,17 @@
-let y = 2;
+// built-in-data-type: string, number, boolean, void, null, undefined
+
+let userId: number;
+let firstName: string;
+let lastName: string;
+let fullName: string;
+let isSkilled: boolean;
+
+userId = 101;
+firstName = 'Mohammad';
+lastName = ' Ibrahim';
+isSkilled = true;
+
+fullName = firstName.concat( lastName);
+console.log(fullName);
+console.log(`My name is ${fullName}`);
+console.log(fullName.split(' '));
