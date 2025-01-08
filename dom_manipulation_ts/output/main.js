eaf8f47bb508aee6d1cf9893cae48e2eb547ba86
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2025-01-08 22:28:42 +0600

    dom manifulation ts live practice

--- dom_manipulation_ts/output/main.js
+++ dom_manipulation_ts/output/main.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log('i am ts');
+const UserForm = document.querySelector('form');
+const UserName = document.querySelector('#user_name');
+const UserEmail = document.querySelector('#user_email');
+const UserCountry = document.querySelector('#country');
+const UserFeedback = document.querySelector('#user_feedback');
+UserForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = {
+        userName: UserName.value,
+        userEmail: UserEmail.value,
+        UserCountry: UserCountry.value,
+        UserFeedback: UserFeedback.value
+    };
+    console.log(data);
+});
--- dom_manipulation_ts/src/main.ts
+++ dom_manipulation_ts/src/main.ts
@@ -1,1 +1,16 @@
-console.log('i am ts');
+const UserForm = document.querySelector('form') as HTMLFormElement;
+const UserName = document.querySelector('#user_name') as HTMLInputElement;
+const UserEmail = document.querySelector('#user_email') as HTMLInputElement;
+const UserCountry = document.querySelector('#country') as HTMLSelectElement;
+const UserFeedback = document.querySelector('#user_feedback') as HTMLTextAreaElement;
+
+UserForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const data = {
+        userName: UserName.value,
+        userEmail: UserEmail.value,
+        UserCountry: UserCountry.value,
+        UserFeedback: UserFeedback.value
+    }
+    console.log(data);
+})
